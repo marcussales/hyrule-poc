d7d40a88703a86ec6350e0e3b04c7581b5ea6a43
Marcus Sales <marcusvinioliversales@gmail.com>
2026-03-06 16:06:55 -0300

    Initial commit

---
+++ lib/screens/results.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:hyrule/controllers/api_controller.dart';
+import 'package:hyrule/screens/components/entry_card.dart';
+import 'package:hyrule/screens/favorites.dart';
+import 'package:hyrule/utils/consts/category_data_consts.dart';
+
+class Results extends StatelessWidget {
+  final String category;
+  Results({super.key, required this.category});
+
+  final ApiController apiController = ApiController();
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+      appBar: AppBar(
+        title: Text(categoriesMap[category]!),
+        actions: [
+          IconButton(
+            onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (context) => Favorites())),
+            icon: const Icon(Icons.bookmark),
+          )
+        ],
+      ),
+      body: FutureBuilder(
+          future: apiController.getEntriesByCategory(category: category),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.active:
+                break;
+              case ConnectionState.waiting:
+                return const Center(child: CircularProgressIndicator());
+              case ConnectionState.none:
+                break;
+              case ConnectionState.done:
+                if (snapshot.hasData) {
+                  return ListView.builder(
+                      itemCount: snapshot.data?.length,
+                      itemBuilder: (context, index) => EntryCard(
+                            entry: snapshot.data![index],
+                            isSaved: false,
+                          ));
+                }
+                break;
+              default:
+            }
+            return Container();
+          }),
+    ));
+  }
+}
